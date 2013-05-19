fd6a5ab792d948f2724921acb7ec0ef19f2b02ba
agius <andrew@atevans.com>
2013-05-19 15:36:56 -0400

    Aliases & things

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -3,5 +3,6 @@ alias r='rbenv local 1.8.7-p358'
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+alias ss='bundle exec rake sunspot:solr:run'
 
 alias migrate='rake db:migrate db:test:clone'
