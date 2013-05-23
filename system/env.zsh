45d36590031c44f20eb15e08aca39b75a8e2468e
agius <andrew@atevans.com>
2013-05-23 16:01:06 -0400

    Cascading editor support

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,9 @@
-export EDITOR='vi'
+if type mate >/dev/null ; then
+  export EDITOR="mate"
+elif type emacs>/dev/null ; then
+  export EDITOR="emacs"
+elif type nano >/dev/null ; then
+  export EDITOR="nano"
+elif type vi >/dev/null ; then
+  export EDITOR="vi"
+fi
