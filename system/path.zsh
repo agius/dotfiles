eab6ef966498b49edd784e17eaa00e81dc47c092
agius <andrew@atevans.com>
2013-10-15 22:25:31 -0700

    Python path for brew-pip

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/libexec:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
