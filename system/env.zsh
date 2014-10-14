77a24ccc8ed29a701ff2220ee9158bde104ea5e7
agius <andrew@atevans.com>
2014-10-14 09:56:13 -0700

    Section off ec2 package vars

--- system/env.zsh
+++ system/env.zsh
@@ -1,18 +1,32 @@
-if type subl >/dev/null ; then
+if which slap &>/dev/null; then
+  export EDITOR='slap'
+elif which subl &>/dev/null; then
   export EDITOR="subl"
-elif type mate >/dev/null ; then
+elif which mate &>/dev/null; then
   export EDITOR="mate"
-elif type vim >/dev/null ; then
+elif which vim &>/dev/null; then
   export EDITOR="vim"
-elif type vi >/dev/null ; then
+elif which vi &>/dev/null; then
   export EDITOR="vi"
-elif type emacs>/dev/null ; then
+elif which emacs &>/dev/null; then
   export EDITOR="emacs"
-elif type nano >/dev/null ; then
+elif which nano &>/dev/null; then
   export EDITOR="nano"
 fi
 
-export JAVA_HOME="$(/usr/libexec/java_home)"
-# export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
-# export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.6.12.0/libexec"
+# 
+# 
+
+if which brew &>/dev/null && brew info ec2-api-tools ; then
+  local ec2_version="${brew info ec2-api-tools | tail +1 | head -1}"
+  export EC2_HOME="/usr/local/Cellar/ec2-api-tools/${ec2_version##*:}/libexec"
+  
+  if [[ -d "/usr/libexec/java_home" ]] ; then
+    export JAVA_HOME="/usr/libexec/java_home"
+  fi
+
+  if [[ -n "$(/bin/ls "$HOME"/.ec2/pk-*.pem)" ]] ; then
+    export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
+    export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
+  fi
+fi
