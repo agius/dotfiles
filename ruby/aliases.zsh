c91b331a6222b26c6e89b870e0c3b3ab643f7c65
agius <andrew@atevans.com>
2015-03-16 11:51:24 -0700

    Unalias chefdk commands from bundler

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,9 @@
 alias rgrep="grep -rEn $1 app lib config spec"
 alias zi="bundle exec zeus start"
 alias zs="bundle exec zeus server"
+
+# should be handled by chefdk
+# brew cask install chefdk
+unalias knife
+unalias foodcritic
+unalias kitchen
