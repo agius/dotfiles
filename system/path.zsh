934f2e09bc09817aa5f757932fe4bf301d8a9f62
agius <andrew@atevans.com>
2015-05-18 09:30:22 -0700

    Add ./node_modules/.bin to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/libexec:$PATH"
+export PATH="./node_modules/.bin/:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/libexec:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
