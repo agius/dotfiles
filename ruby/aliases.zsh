60e3daa5f1dc3153ca97bae4d7f81b9afe7aee61
agius <andrew@atevans.com>
2015-07-20 10:16:03 -0700

    Zeus kill && update ffs

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,6 +1,7 @@
 alias rgrep="grep -rEn $1 app lib config spec"
 alias zi="bundle exec zeus start"
 alias zs="bundle exec zeus server"
+alias zk="killall -m zeus"
 
 # should be handled by chefdk
 # brew cask install chefdk
