dfb09bd64f078b8fa1ad76ed181945c3b3b33682
agius <andrew@atevans.com>
2015-11-26 11:46:13 -0800

    Add subl to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./node_modules/.bin/:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/libexec:$PATH"
+export PATH="./node_modules/.bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/libexec:/Applications/Sublime Text.app/Contents/SharedSupport/bin:~/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
