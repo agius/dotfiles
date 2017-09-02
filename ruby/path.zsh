2ed93c26d9f0c757df5873dde453e4bfa143d36b
agius <andrew@atevans.com>
2017-09-02 12:00:34 -0700

    Basic fixes, add editorconfig
    Skip pyenv for now, set home dir for rbenv, and add a global .editorconfig

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,2 +1,6 @@
 # handled by antigen plugin
 # export PATH="$HOME/.rbenv/shims:$PATH"
+
+# antigen rbenv plugin overrides this to reside in /usr/local/Cellar/rbenv
+# which means homebrew can screw with rbenv's settings - we don't want that
+export RBENV_ROOT=~/.rbenv
