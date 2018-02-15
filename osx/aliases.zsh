f1bdf4274f244f253247d9025ea3ef88c1677c5f
agius <andrew@atevans.com>
2018-02-15 11:46:23 -0800

    homebrew whois

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -7,3 +7,9 @@ if [[ $(uname -s) == 'Darwin' ]] ; then
     alias typora="open -a Typora $1"
   fi
 fi
+
+# if we've installed debian whois via homebrew, use that
+# instead of the native Mac whois
+if [[ -f /usr/local/bin/whois ]] ; then
+  alias whois='/usr/local/bin/whois'
+fi
