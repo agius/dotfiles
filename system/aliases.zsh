15e477de989e54942d06e9a38ede9af3184bc432
agius <andrew@atevans.com>
2018-08-30 11:36:25 -0700

    Git get default branch; cli burp suite launcher

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -11,3 +11,7 @@ fi
 if [[ -f /usr/local/bin/howdoi ]] ; then
   alias h='function hdi(){ howdoi $* -c -n 5; }; hdi'
 fi
+
+if [[ -d /Applications/Burp\ Suite\ Community\ Edition.app ]] ; then
+  alias burp='java -jar -Xmx1024m /Applications/Burp\ Suite\ Community\ Edition.app/Contents/java/app/burpsuite_community.jar'
+fi
