6b67c2ab7c6feec741962c9dfb9462f4359b54d6
agius <andrew@atevans.com>
2018-10-16 09:30:04 -0400

    Fix qt@5.5 path issue

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -4,6 +4,7 @@
 # antigen rbenv plugin overrides this to reside in /usr/local/Cellar/rbenv
 # which means homebrew can screw with rbenv's settings - we don't want that
 export RBENV_ROOT=~/.rbenv
+
 if brew ls --versions qt@5.5 > /dev/null; then
   export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
 fi
