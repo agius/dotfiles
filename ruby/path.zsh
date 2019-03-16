e9a7fdbfae37a63a150ae00cd7f102d550ce7d9e
agius <andrew@atevans.com>
2019-03-16 12:52:27 -0700

    Fix qt@5.5 hack for Homebrew

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -4,4 +4,6 @@
 # antigen rbenv plugin overrides this to reside in /usr/local/Cellar/rbenv
 # which means homebrew can screw with rbenv's settings - we don't want that
 export RBENV_ROOT=~/.rbenv
-export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
+if brew ls --versions qt@5.5 > /dev/null; then
+  export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
+fi
