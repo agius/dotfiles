ac455e17ecbdcf727da213c83d7264eb233136ac
agius <andrew@atevans.com>
2019-10-16 12:08:29 +0200

    Update EDITOR, gemrc, gitignore

--- system/env.zsh
+++ system/env.zsh
@@ -1,7 +1,7 @@
-if which vim &>/dev/null; then
+if which subl &>/dev/null; then
+  export EDITOR="subl --new-window --wait"
+elif which vim &>/dev/null; then
   export EDITOR="vim"
-elif which subl &>/dev/null; then
-  export EDITOR="subl"
 elif which slap &>/dev/null; then
   export EDITOR='slap'
 elif which emacs &>/dev/null; then
@@ -10,8 +10,4 @@ elif which vi &>/dev/null; then
   export EDITOR="vi"
 elif which nano &>/dev/null; then
   export EDITOR="nano"
-fi
-
-if [[ -n $RBENV_VERSION ]] ; then
-  export RBENV_VERSION=''
 fi
