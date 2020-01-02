6ecfc6bf899834d44c28f13862382ace7575247d
agius <andrew@atevans.com>
2020-01-02 09:17:03 -0500

    Add brew libicu path for yarn / node

--- osx/path.zsh
+++ osx/path.zsh
@@ -1,1 +1,6 @@
 export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin:~/Applications/Sublime Text.app/Contents/SharedSupport/bin:$PATH"
+
+if [ -d /usr/local/opt/icu4c/bin ] ; then
+  export PATH="/usr/local/opt/icu4c/bin:$PATH"
+  export PATH="/usr/local/opt/icu4c/sbin:$PATH"
+fi
