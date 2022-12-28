123cf5d7ef99db33c361ceeda385976e9c622b0a
Andrew 'Agius' Evans <andrew.evans@trustedhealth.com>
2022-12-28 10:07:34 -0800

    Fix objc forking issue in Ruby

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,6 +1,11 @@
 # handled by antigen plugin
 # export PATH="$HOME/.rbenv/shims:$PATH"
 
+# fixes a recurring issue with ruby/rails loading objc libs after fork
+# see https://github.com/rails/rails/issues/38560
+# and https://bugs.ruby-lang.org/issues/14009#note-4
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
 # antigen rbenv plugin overrides this to reside in /usr/local/Cellar/rbenv
 # which means homebrew can screw with rbenv's settings - we don't want that
 export RBENV_ROOT=~/.rbenv
